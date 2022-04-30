44a0014382e37c6c6ebab058a4fccb01ee350192
Vlad Rusakov <24524555+wervlad@users.noreply.github.com>
2022-04-30 18:45:06 +0400

    add train cli

---
+++ src/forestml/train.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import click
+import pandas as pd
+
+@click.command()
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path)
+)
+def train(
+    dataset_path: Path
+) -> None:
+    dataset = pd.read_csv(dataset_path)
+    click.echo(f"Dataset shape: {dataset.shape}.")
