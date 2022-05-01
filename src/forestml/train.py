1842a167e6c7b75936ae4eb4401150cbc74575b5
Vlad Rusakov <24524555+wervlad@users.noreply.github.com>
2022-05-01 07:39:41 +0400

    format code with black

--- src/forestml/train.py
+++ src/forestml/train.py
@@ -2,15 +2,14 @@ from pathlib import Path
 import click
 import pandas as pd
 
+
 @click.command()
 @click.option(
     "-d",
     "--dataset-path",
     default="data/train.csv",
-    type=click.Path(exists=True, dir_okay=False, path_type=Path)
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
 )
-def train(
-    dataset_path: Path
-) -> None:
+def train(dataset_path: Path) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
