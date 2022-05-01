15b9846d8861b870d19a7ca242dd7731a9d578ba
Vlad Rusakov <24524555+wervlad@users.noreply.github.com>
2022-05-01 13:07:02 +0400

    add code for training logistic regression

--- src/forestml/train.py
+++ src/forestml/train.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 import click
 import pandas as pd
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 @click.command()
 @click.option(
@@ -13,3 +15,11 @@ import pandas as pd
 def train(dataset_path: Path) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
+    features = dataset.drop("Cover_Type", axis=1)
+    target = dataset["Cover_Type"]
+    features_train, features_val, target_train, target_val = train_test_split(
+        features, target, test_size=0.2, random_state=42
+    )
+    classifier = LogisticRegression(random_state=42).fit(features_train, target_train)
+    accuracy = accuracy_score(target_val, classifier.predict(features_val))
+    click.echo(f"Accuracy: {accuracy}.")
