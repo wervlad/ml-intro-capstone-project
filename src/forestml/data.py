fe64413e6ecd858e7d0aa8ddd36fbf37c3038777
Vlad Rusakov <24524555+wervlad@users.noreply.github.com>
2022-05-03 14:14:56 +0400

    add pandas-profiling report

--- src/forestml/data.py
+++ src/forestml/data.py
@@ -4,13 +4,43 @@ from typing import Tuple
 
 import click
 import pandas as pd
+import pandas_profiling
 
 DATASET_PATH = "data/train.csv"
+REPORT_PATH = "data/report.html"
 TARGET = "Cover_Type"
 
-def get_dataset(csv_path: Path) -> Tuple[pd.DataFrame, pd.Series]:
+def get_dataset(
+    csv_path: Path,
+    return_X_y: bool = True,
+) -> Tuple[pd.DataFrame, pd.Series]:
     dataset = pd.read_csv(csv_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
-    features = dataset.drop(TARGET, axis=1)
-    target = dataset[TARGET]
-    return features, target
+    if return_X_y:
+        features = dataset.drop(TARGET, axis=1)
+        target = dataset[TARGET]
+        return features, target
+    else:
+        return dataset
+
+@click.command()
+@click.option(
+    "-i",
+    "--csv-path",
+    default=DATASET_PATH,
+    show_default=True,
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+)
+@click.option(
+    "-o",
+    "--report-path",
+    default=REPORT_PATH,
+    show_default=True,
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+)
+def generate_profiling_report(csv_path: Path, report_path: Path) -> None:
+    profile = pandas_profiling.ProfileReport(
+        get_dataset(csv_path, return_X_y=False),
+        minimal=False
+    )
+    profile.to_file(report_path)
