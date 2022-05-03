884336b462ce7a7fae5ad881e58d160fcad35f3a
Vlad Rusakov <24524555+wervlad@users.noreply.github.com>
2022-05-03 12:56:24 +0400

    refactor: eliminate premature organization

--- src/forestml/train.py
+++ src/forestml/train.py
@@ -1,17 +1,20 @@
-from math import inf
-from pathlib import Path
-from joblib import dump
 import click
 import mlflow
 import mlflow.sklearn
 import pandas as pd
+from math import inf
+from joblib import dump
+from pathlib import Path
+from sklearn.base import BaseEstimator
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from sklearn.model_selection import KFold
-from .data import get_dataset
-from .pipeline import create_pipeline
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from typing import Tuple
 
-@click.command()
+@click.group()
+@click.pass_context
 @click.option(
     "-d",
     "--dataset-path",
@@ -39,27 +42,67 @@ from .pipeline import create_pipeline
     show_default=True,
     type=bool,
 )
-@click.option("--max-iter", default=100, show_default=True, type=int)
-@click.option("--logreg-c", default=1.0, show_default=True, type=float)
 def train(
+    ctx: click.core.Context,
     dataset_path: Path,
     save_model_path: Path,
     random_state: int,
     n_splits: int,
     use_scaler: bool,
+) -> None:
+    ctx.ensure_object(dict)
+    dataset = pd.read_csv(dataset_path)
+    click.echo(f"Dataset shape: {dataset.shape}.")
+    ctx.obj["X"] = dataset.drop("Cover_Type", axis=1)
+    ctx.obj["y"] = dataset["Cover_Type"]
+    ctx.obj["save_model_path"] = save_model_path
+    ctx.obj["random_state"] = random_state
+    ctx.obj["n_splits"] = n_splits
+    ctx.obj["use_scaler"] = use_scaler
+
+@train.command()
+@click.pass_context
+@click.option("--max-iter", default=100, show_default=True, type=int)
+@click.option("--c", default=1.0, show_default=True, type=float)
+def logreg(
+    ctx: click.core.Context,
     max_iter: int,
-    logreg_c: float,
+    c: float,
+) -> None:
+    ctx.obj["max_iter"] = max_iter
+    ctx.obj["c"] = c
+    model = LogisticRegression(
+        random_state=ctx.obj["random_state"], max_iter=max_iter, C=c
+    )
+    run_experiment(
+        ctx,
+        create_pipeline(use_scaler=ctx.obj["use_scaler"], model=model),
+    )
+
+
+def create_pipeline(
+    use_scaler: bool, model: BaseEstimator
+) -> Pipeline:
+    pipeline_steps = []
+    if use_scaler:
+        pipeline_steps.append(("scaler", StandardScaler()))
+    pipeline_steps.append(("classifier", model))
+    return Pipeline(steps=pipeline_steps)
+
+def run_experiment(
+    ctx: click.core.Context,
+    pipeline: Pipeline,
 ) -> None:
     with mlflow.start_run():
         accuracy_list = []
         precision_list = []
         recall_list = []
         f1_list = []
-        X, y = get_dataset(dataset_path)
-        X = X.to_numpy()
-        y = y.to_numpy()
-        kf = KFold(n_splits=n_splits, shuffle=True, random_state=random_state)
-        pipeline = create_pipeline(use_scaler, max_iter, logreg_c, random_state)
+        X = ctx.obj["X"].to_numpy()
+        y = ctx.obj["y"].to_numpy()
+        kf = KFold(n_splits=ctx.obj["n_splits"],
+                   shuffle=True,
+                   random_state=ctx.obj["random_state"])
         # use KFold cross-validator to calculate metrics
         for train_indices, test_indices in kf.split(X, y):
             X_train = X[train_indices]
@@ -76,9 +119,9 @@ def train(
             recall_list.append(recall)
             f1_list.append(f1)
         pipeline.fit(X, y)  # finally train model on whole dataset
-        mlflow.log_param("use_scaler", use_scaler)
-        mlflow.log_param("max_iter", max_iter)
-        mlflow.log_param("logreg_c", logreg_c)
+        mlflow.log_param("use_scaler", ctx.obj["use_scaler"])
+        mlflow.log_param("max_iter", ctx.obj["max_iter"])
+        mlflow.log_param("logreg_c", ctx.obj["c"])
         mlflow.log_metric("accuracy", sum(accuracy_list) / len(accuracy_list))
         mlflow.log_metric("precision", sum(precision_list) / len(precision_list))
         mlflow.log_metric("recall", sum(recall_list) / len(recall_list))
@@ -87,5 +130,5 @@ def train(
         click.echo(f"Precision: {precision}.")
         click.echo(f"Recall: {recall}.")
         click.echo(f"F1: {f1}.")
-        dump(pipeline, save_model_path)
-        click.echo(f"Model is saved to {save_model_path}.")
+        dump(pipeline, ctx.obj["save_model_path"])
+        click.echo(f"Model is saved to {ctx.obj['save_model_path']}.")
