4bfd7359a2d5ea3168bcdb46a68d4f848cc211ea
Vlad Rusakov <24524555+wervlad@users.noreply.github.com>
2022-05-03 14:23:25 +0400

    drop constant columns

--- src/forestml/data.py
+++ src/forestml/data.py
@@ -9,13 +9,17 @@ import pandas_profiling
 DATASET_PATH = "data/train.csv"
 REPORT_PATH = "data/report.html"
 TARGET = "Cover_Type"
+DROP = ["Soil_Type7", "Soil_Type15"]
 
 def get_dataset(
     csv_path: Path,
-    return_X_y: bool = True,
+    return_X_y: bool=True,
+    drop_columns: bool=True,
 ) -> Tuple[pd.DataFrame, pd.Series]:
     dataset = pd.read_csv(csv_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
+    if drop_columns:
+        dataset.drop(DROP, axis=1, inplace=True)
     if return_X_y:
         features = dataset.drop(TARGET, axis=1)
         target = dataset[TARGET]
@@ -40,7 +44,7 @@ def get_dataset(
 )
 def generate_profiling_report(csv_path: Path, report_path: Path) -> None:
     profile = pandas_profiling.ProfileReport(
-        get_dataset(csv_path, return_X_y=False),
+        get_dataset(csv_path, return_X_y=False, drop_columns=False),
         minimal=False
     )
     profile.to_file(report_path)
